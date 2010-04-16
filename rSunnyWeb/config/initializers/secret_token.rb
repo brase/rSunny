02f4ebd4e3215c90b2d8048185dea30220fce0d0
brase <brase@brase-sabayon.(none)>
2010-04-16 19:59:08 +0200

    added rails3b3 webapp

---
+++ rSunnyWeb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '3497a2adb8ef6a71d7c08c5ca3b6c6e797947dffdfe078eda8190f4137806a5689342644c019da8e9816148be10a459aed2e5bf2b1034d53d3d417d5c39afac2'
